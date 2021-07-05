8be45d63726711fb7c03c0ea7ab6fa3e9fa66977
Triang-jyed-driung <62597758+Triang-jyed-driung@users.noreply.github.com>
2021-07-05 20:35:50 +0800

    Add files via upload

---
+++ markov.py
@@ -0,0 +1,44 @@
+import random
+import jieba
+articlelength=1000
+txt=[]
+with open("in.txt","r",encoding="utf8") as fp:
+    txt=fp.read()
+words=jieba.lcut(txt)
+print(words)
+M={}
+for i in range(2, len(words)):
+    s1 = words[i-2] + '`' + words[i-1]
+    s2 = words[i]
+    if s1 not in M:
+        M[s1] = {}
+        M[s1]['`'] = 0
+    if s2 not in M[s1]:
+        M[s1][s2] = 0
+    M[s1]['`'] += 1
+    M[s1][s2] += 1
+print(M)
+print(len(M))
+# beginning
+a = random.randint(1, len(words)-1)
+state = words[a-1] + '`' + words[a]
+with open("out.txt", "w", encoding="utf8") as fpw:
+    fpw.write(words[a-1] + words[a])
+    for i in range(0, articlelength):
+        pos = random.randint(1, M[state]['`'])
+        cnt = 0
+        for nextword in M[state]:
+            if nextword != '`':
+                cnt+=M[state][nextword]
+                if(cnt >= pos):
+                    fpw.write(nextword)
+                    state = state.split('`')[1] + '`' + nextword
+                    break
+        
+
+
+
+
+
+    
+
